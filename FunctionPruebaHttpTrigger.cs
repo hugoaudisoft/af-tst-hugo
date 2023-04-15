0b3e2f2121b4bbaa59d850327fddef558b953254
DevH <hugo.garciag4@gmail.com>
2023-04-14 23:32:38 -0500

    archivos nuevos

---
+++ FunctionPruebaHttpTrigger.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace My.Function
+{
+    public class FunctionPruebaHttpTrigger
+    {
+        private readonly ILogger _logger;
+
+        public FunctionPruebaHttpTrigger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<FunctionPruebaHttpTrigger>();
+        }
+
+        [Function("FunctionPruebaHttpTrigger")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
